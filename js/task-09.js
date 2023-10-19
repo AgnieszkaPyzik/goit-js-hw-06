518b7a16b2ef6d4cf09b1c656949ef1a3d1f37ff
AgnieszkaPyzik <agnieszka.piescik@gmail.com>
2023-10-19 23:19:21 +0200

    HW corrections

--- js/task-09.js
+++ js/task-09.js
@@ -7,7 +7,7 @@ const buttonColor = document.querySelector(".change-color");
 const spanColor = document.querySelector(".color");
 
 buttonColor.addEventListener("click", () => {
-  let color = getRandomHexColor(); //losowy kolor w formacie HEX z funkcji
-  document.body.style.backgroundColor = color; // zmiana koloru tła elementu <body>
-  spanColor.textContent = color; // wyświetlenie wartości koloru w elemencie <span> o klasie "color"
+  let color = getRandomHexColor(); 
+  document.body.style.backgroundColor = color; 
+  spanColor.textContent = color; 
 });
